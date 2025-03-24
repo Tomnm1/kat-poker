01e1192f7558af1648ad231e9a483c95983ed246
Tomnm1 <tomnm77@gmail.com>
2025-03-24 10:15:00 +0100

    init frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-blue-200 flex text-center items-center justify-center min-h-screen text-5xl text-gray-800">
+      test
+    </div>
+  );
+}
