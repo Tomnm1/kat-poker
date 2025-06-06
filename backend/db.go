0b49db24106d31b8b6030fdfed22a13cebb6261d
Ssyhaj <118054400+Ssyhaj@users.noreply.github.com>
2025-06-07 00:21:10 +0200

    Update db.go

--- backend/db.go
+++ backend/db.go
@@ -4,7 +4,30 @@ import (
 	"context"
 	"log"
 	"time"
-	"os"
+func initMongoDB() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var err error
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		mongoURI = "mongodb://mongo:mongo@localhost:27017"
+	}
+
+	clientOptions := options.Client().ApplyURI(mongoURI)
+	client, err = mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatalf("MongoDB connection error: %v", err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("MongoDB ping error: %v", err)
+	}
+
+	sessionCol = client.Database("planning").Collection("sessions")
+	log.Println("Connected to MongoDB")
+}
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
