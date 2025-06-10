a5855555f974b16a81309d2635a26a9879dfc8d3
Ssyhaj <118054400+Ssyhaj@users.noreply.github.com>
2025-06-10 16:10:20 +0200

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -11,7 +11,6 @@ import (
 
 func main() {
 	initMongoDB()
-	// Set up CORS options
 	corsOptions := cors.New(cors.Options{
 		AllowedOrigins:   []string{"http://localhost:3000", "https://kat-poker.vercel.app"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
@@ -19,14 +18,11 @@ func main() {
 		AllowCredentials: true,
 	})
 
-	// Initialize the router
 	r := mux.NewRouter()
-	registerRoutes(r) // Register the routes from the handlers file
+	registerRoutes(r)
 
-	// Apply CORS middleware
 	handler := corsOptions.Handler(r)
 
-	// Start the server with CORS enabled
 	log.Println("Server is running on port :8080")
 	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatalf("Error starting server: %v", err)
