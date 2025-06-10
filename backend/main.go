a10247a5363a024610164c1c5252ce62c70fe622
Ssyhaj <118054400+Ssyhaj@users.noreply.github.com>
2025-06-10 14:57:01 +0200

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -13,11 +13,12 @@ func main() {
 	initMongoDB()
 	// Set up CORS options
 	corsOptions := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedOrigins:   []string{"http://localhost:3000", "https://kat-poker.vercel.app"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
 		AllowedHeaders:   []string{"Content-Type", "Authorization"},
 		AllowCredentials: true,
 	})
+
 
 	// Initialize the router
 	r := mux.NewRouter()
