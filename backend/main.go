49db00bd1c904b49afd2188edbadbcbbd9a05ce8
Ssyhaj <118054400+Ssyhaj@users.noreply.github.com>
2025-06-10 16:31:25 +0200

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -11,22 +11,27 @@ import (
 
 func main() {
 	initMongoDB()
-	// Set up CORS options
 	corsOptions := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000", "https://kat-poker.vercel.app"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
-		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowedOrigins: []string{
+			"http://localhost:3000",
+			"https://kat-poker.vercel.app",
+			"http://kat-poker.vercel.app",
+			"kat-poker.vercel.app",
+			"https://kat-poker.vercel.app/*",
+			"http://kat-poker.vercel.app/*",
+			"kat-poker.vercel.app/*",
+		},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "WEBSOCKET"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization", "Application", "application/json"},
 		AllowCredentials: true,
 	})
 
-	// Initialize the router
-	r := mux.NewRouter()
-	registerRoutes(r) // Register the routes from the handlers file
 
-	// Apply CORS middleware
+	r := mux.NewRouter()
+	registerRoutes(r) 
+
 	handler := corsOptions.Handler(r)
 
-	// Start the server with CORS enabled
 	log.Println("Server is running on port :8080")
 	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatalf("Error starting server: %v", err)
